bf0dc6ba4557fdfec328b8f9450c6f7c76545db1
REDEXCT <121300580+REDEXCT@users.noreply.github.com>
2023-09-30 17:15:21 -0400

    Update ChatInput.tsx

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -386,11 +386,11 @@ export const ChatInput = ({
           rel="noreferrer"
           className="underline"
         >
-          ChatBot UI
+          Jarvis
         </a>
         .{' '}
         {t(
-          "Chatbot UI is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
+          "Jarvis is an independently owned model created by REDEX LLC.",
         )}
       </div>
     </div>
