29591b6559479bfa4e4dbde90a34794dd6299b92
REDEXCT <121300580+REDEXCT@users.noreply.github.com>
2024-04-14 23:43:36 -0400

    Update openai.ts

--- types/openai.ts
+++ types/openai.ts
@@ -22,34 +22,10 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     maxLength: 12000,
     tokenLimit: 4000,
   },
-  [OpenAIModelID.GPT_3_5_AZ]: {
-    id: OpenAIModelID.GPT_3_5_AZ,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
-  },
   [OpenAIModelID.GPT_4]: {
     id: OpenAIModelID.GPT_4,
     name: 'GPT-4',
     maxLength: 4096,
     tokenLimit: 128,000,
-  },
-  [OpenAIModelID.GPT_4_32K]: {
-    id: OpenAIModelID.GPT_4_32K,
-    name: 'GPT-4-32K',
-    maxLength: 96000,
-    tokenLimit: 32000,
-  },
-  [OpenAIModelID.GPT_4_1106_PREVIEW]: {
-    id: OpenAIModelID.GPT_4_1106_PREVIEW,
-    name: 'GPT-4-1106-Preview',
-    maxLength: 4096, // as per the context window
-    tokenLimit: 128000, // as per the training data
-  },
-  [OpenAIModelID.GPT_4_VISION_PREVIEW]: {
-    id: OpenAIModelID.GPT_4_VISION_PREVIEW,
-    name: 'GPT-4-Vision-Preview',
-    maxLength: 4096, // as per the context window
-    tokenLimit: 128000, // as per the training data
   },
 };
